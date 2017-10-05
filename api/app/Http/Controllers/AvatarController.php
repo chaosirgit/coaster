7f288bced3d5716e29f7edc1e028334712b88bc5
chaosirgit <qq278935403@gmail.com>
2017-10-05 21:06:45 +0800

    10082106

--- api/app/Http/Controllers/AvatarController.php
+++ api/app/Http/Controllers/AvatarController.php
@@ -12,22 +12,26 @@ class AvatarController extends Controller
             $image = $request->input('image');
             $istoken = DB::select('select * from token where user_token=:token',['token'=>substr($token,7)]);
             if($istoken){
-
-                $row =	DB::update('update user set image=:image where id=:id',['image'=>$image,'id'=>$istoken[0]->uid]);
-                if($row){
+                $result = DB::select('select image from user where id=:id',['id'=>$istoken[0]->uid]);
+                if($image == $result[0]->image)
+                {
                     return response('',200)->header('Content-Type','text/html;charset=utf-8');
-                }else{
-                    return response('',500)->header('Content-Type','text/html;charset=utf-8');
+                }else {
+                    $row = DB::update('update user set image=:image where id=:id', ['image' => $image, 'id' => $istoken[0]->uid]);
+                    if ($row) {
+                        return response('', 200)->header('Content-Type', 'text/html;charset=utf-8');
+                    } else {
+                        return response('', 500)->header('Content-Type', 'text/html;charset=utf-8');
+                    }
                 }
-
             }else{
                 return response('',401)->header('Content-Type','text/html;charset=utf-8');
             }
         }else{
             return response('',500)->header('Content-Type','text/html;charset=utf-8');
         }
-
     }
+
     public function get_image(Request $request,$uid){
         if ($request->isMethod('get')){
             $arr = DB::select('select image from user where id=:id',['id'=>$uid]);
